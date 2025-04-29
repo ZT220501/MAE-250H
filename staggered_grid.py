7829505a84b2dadd25322409c06bb5a7c7919f95
ZT220501 <zheng07@g.ucla.edu>
2025-04-29 13:28:03 -0700

    Velocity pointer implementation

--- staggered_grid.py
+++ staggered_grid.py
@@ -11,7 +11,6 @@ from operators import vorticity, divergence, gradient
 '''
 Generate the staggered grid.
 The position of the variables have been verified.
-TODO: Test the operator implementations.
 '''
 class StaggeredGrid:
 
@@ -54,7 +53,10 @@ class StaggeredGrid:
         self.pressure = initial_condition_pressure(self.pressure_mesh_grid)
 
         # Define the velocity field at the faces using the initial condition
+        # self.velocity contains the stacked u, v at the non-boundary faces.
         self.u, self.v = initial_condition_velocity(self.u_mesh_grid, self.v_mesh_grid)
+        self.velocity = np.stack((self.u.reshape(-1), self.v.reshape(-1)), axis=0)
+        self.u_pointer, self.v_pointer = self.pointer_velocity()
 
         # Compute the vorticity at the cell centers based on the velocity field
         self.vorticity = vorticity(self.u, self.v, self.vorticity_mesh_grid)
@@ -91,12 +93,45 @@ class StaggeredGrid:
         return self.pressure
     # Get the velocities at the faces
     def get_velocity(self):
-        return self.u, self.v
+        return self.u, self.v, self.velocity
     # Get the vorticity at the cell vertices
     def get_vorticity(self):
         return self.vorticity
     
 
+    # Pointer functions
+    def pointer_velocity(self):
+        '''
+        Generate a map that maps the 2D index of the 
+        velocity field to the 1D index of the velocity field.
+        '''
+        idx = 0
+        u_pointer = np.zeros(self.u.shape)
+        v_pointer = np.zeros(self.v.shape)
+
+        for i in range(self.u.shape[0]):
+            for j in range(self.u.shape[1]):
+                u_pointer[i, j] = idx
+                idx += 1
+        # We do NOT reset the idx in between.
+        for i in range(self.v.shape[0]):
+            for j in range(self.v.shape[1]):
+                v_pointer[i, j] = idx
+                idx += 1
+
+        return u_pointer, v_pointer
+    
+    def pointer_pressure(self):
+        '''
+        Generate a map that maps the 2D index of the 
+        pressure field to the 1D index of the pressure field.
+        TODO: Finish this.
+        '''
+        pass
+
+    
+    
+    # Visualization functions
     def visualize_velocity(self, scale=0.5):
         '''
         Visualize the x and y components of the velocity field
