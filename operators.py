83a307522221b938cea1be9a22ce4826c299e5cf
ZT220501 <zheng07@g.ucla.edu>
2025-04-29 13:52:27 -0700

    More modifications

--- operators.py
+++ operators.py
@@ -159,40 +159,5 @@ def nonlinear_advection(u_previous, mesh_grid, V0, vector_field):
     dt: the time step
     V0: the vector field strength
     vector_field: the vector field
-
-
-    TODO: Finish this part for general nonlinear advection instead of simply two cases
     '''
-    x_grid, y_grid = mesh_grid
-
-    # Get the grid spacing
-    dx = x_grid[0, 1] - x_grid[0, 0]
-    dy = y_grid[1, 0] - y_grid[0, 0]
-
-
-
-
-    # Implement the Engquist-Osher flux
-    # Notice that the vector field is convex in each component in both cases
-    if vector_field == "expanding":
-        # Expanding flow case vector field \vec{v}=(V_0x, V_0y)
-        x_faces = 0.5 * (x_grid[1:-1, 1:] + x_grid[1:-1, :-1])
-        F_x = enquist_osher_flux_expanding(u_previous[1:-1, :-1], u_previous[1:-1, 1:], V0, x_faces)
-        y_faces = 0.5 * (y_grid[1:, 1:-1] + y_grid[:-1, 1:-1])
-        F_y = enquist_osher_flux_expanding(u_previous[:-1, 1:-1], u_previous[1:, 1:-1], V0, y_faces)
-
-        # Finite volume update
-        u_convection = (F_x[:, 1:] - F_x[:, :-1]) / dx + (F_y[1:, :] - F_y[:-1, :]) / dy
-    elif vector_field == "sheer":
-        # Sheer flow case vector field \vec{v}=(0, -V_0x)
-        # TODO: Implement the sheer flow case using the Engquist-Osher flux
-        vy_positive = np.maximum(0, -V0 * x_grid[1:-1, 1:-1])
-        vy_negative = np.minimum(0, -V0 * x_grid[1:-1, 1:-1])
-        uy_positive = (u_previous[2:, 1:-1] - u_previous[1:-1, 1:-1]) / dy
-        uy_negative = (u_previous[1:-1, 1:-1] - u_previous[:-2, 1:-1]) / dy
-        u_convection = vy_positive * uy_negative + vy_negative * uy_positive
-    else:
-        raise ValueError("Invalid vector field.")   
-
-
-    return u_convection
+    pass
